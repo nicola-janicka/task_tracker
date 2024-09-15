49857860535cb71006d7956cb31d55aa4cec70dd
nicola-janicka <janicka.nicola@gmail.com>
2024-09-15 17:59:40 +0200

    Redirect after login

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { TaskTableComponent } from '../task-table/task-table.component';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    RouterOutlet,
+    TaskTableComponent,
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
+  constructor(private router: Router) {}
   username: string = '';
   password: string = '';
 
   onSubmit() {
     console.log('Username:', this.username);
     console.log('Password:', this.password);
+    this.router.navigate(['/task-table']);
   }
 }
