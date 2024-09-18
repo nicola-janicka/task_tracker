5d55510d6f8dbeb6ada3aab41a1a5328f7a15922
nicola-janicka <janicka.nicola@gmail.com>
2024-09-18 17:59:29 +0200

    Added simple authentication and login page styled

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { TaskTableComponent } from '../task-table/task-table.component';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { MatInput } from '@angular/material/input';
+import { MatInput, MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login-page',
@@ -21,6 +22,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatInput,
     MatFormFieldModule,
     MatButtonModule,
+    MatInputModule,
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css',
@@ -30,9 +32,16 @@ export class LoginPageComponent {
   username: string = '';
   password: string = '';
 
+  adminUser: string = 'nicola';
+  adminPass: string = 'alocin';
+
+  private _snackBar = inject(MatSnackBar);
   onSubmit() {
-    console.log('Username:', this.username);
-    console.log('Password:', this.password);
-    this.router.navigate(['/task-table']);
+    if (this.username === this.adminUser && this.password === this.adminPass) {
+      localStorage.setItem('logged', 'true');
+      this.router.navigate(['/task-table']);
+    }
+
+    this._snackBar.open('login or password is incorrect', 'OK');
   }
 }
