b7f34ed7b542b59dccf281e3f7a2ba4aea96317f
nicola-janicka <janicka.nicola@gmail.com>
2024-11-04 19:00:49 +0100

    fix

--- src/app/admin-login-page/admin-login-page.component.ts
+++ src/app/admin-login-page/admin-login-page.component.ts
@@ -1,4 +1,3 @@
-import { adminService } from './../adminService';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
