84881517bf75f3913561fc6670d94c745f0c5600
nicola-janicka <janicka.nicola@gmail.com>
2024-11-20 22:03:12 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { testMe } from './testest';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
-
-testMe();
--- src/testest.ts
+++ src/testest.ts
@@ -1,22 +1,22 @@
-import { from } from 'rxjs';
+// import { from } from 'rxjs';
 
-//Pobieranie danych z json-server:
-async function getContacts() {
-  const response = await fetch('http://localhost:3000/contacts', {
-    method: 'GET',
-  });
-  const jsonContacts = await response.json();
-  return jsonContacts; //funkcja zwraca Promise'a
-}
+// //Pobieranie danych z json-server:
+// async function getContacts() {
+//   const response = await fetch('http://localhost:3000/contacts', {
+//     method: 'GET',
+//   });
+//   const jsonContacts = await response.json();
+//   return jsonContacts; //funkcja zwraca Promise'a
+// }
 
-export function testMe() {
-  // Przypisuję wynik funkcji do zmiennej, która jest moim promisem
-  let contacts = getContacts();
+// export function testMe() {
+//   // Przypisuję wynik funkcji do zmiennej, która jest moim promisem
+//   let contacts = getContacts();
 
-  //Zamiana Promise'a w Observable
-  const contactsObservable = from(contacts);
+//   //Zamiana Promise'a w Observable
+//   const contactsObservable = from(contacts);
 
-  contactsObservable.subscribe((value) => {
-    console.log(value);
-  });
-}
+//   contactsObservable.subscribe((value) => {
+//     console.log(value);
+//   });
+// }
